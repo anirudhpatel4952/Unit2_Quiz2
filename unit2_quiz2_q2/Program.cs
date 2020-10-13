83ff760600e93659907025511636e86dd2c60d43
Anirudhkumar Patel <anirudhpatel4952@gmail.com>
2020-10-12 21:56:52 -0600

    This have the answer for the program of unit 2 quiz 2 ques 2

---
+++ unit2_quiz2_q2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace unit2_quiz2_q2
+{
+    class quiz2q2
+    {
+        static void Main(string[] args)
+        {
+        //     /*Write a function that takes in a string as input, removes the vowels from the string (aeiou), 
+        //     and then returns that new string as the output. Example: I input "Hello World" - the result 
+        //     should be "Hll wrld".*/
+        
+        
+        Console.WriteLine("Enter the string");
+            var statement = Console.ReadLine();
+            var newStatement = NoVowel(statement);
+            Console.WriteLine(newStatement);
+        }
+
+
+        static string NoVowel(string str)
+        {
+            var sentSplit = str.ToCharArray();
+            var vowelString = new List<char>()
+                { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            string newStatement = "";
+            for (int i = 0; i < sentSplit.Length; i++)
+            {
+                char v = sentSplit[i];
+                if (vowelString.Contains(v))
+                {
+                    continue;
+                }
+                else
+                {
+                    newStatement += v;
+                }
+            }
+            return newStatement;
+        }
+        
+        
+        
+        
+
+
+
+    }
+}
